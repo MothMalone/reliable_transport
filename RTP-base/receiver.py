c695f2f0a14d2af10244dd08aed55e79eec10c1b
MothMalone <nam28042004@gmail.com>
2025-04-30 16:10:08 +0700

    Initialize UDP protocol & START handshake

--- RTP-base/receiver.py
+++ RTP-base/receiver.py
@@ -1,45 +1,38 @@
 import argparse
 import socket
-
+import sys
 from utils import PacketHeader, compute_checksum
 
-
 def receiver(receiver_ip, receiver_port, window_size):
-    """TODO: Listen on socket and print received message to sys.stdout."""
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((receiver_ip, receiver_port))
+    print(f"Receiver listening on {receiver_ip}:{receiver_port}\n", file=sys.stderr)
+
+    connection_active = False
+
     while True:
-        # Receive packet; address includes both IP and port
-        pkt, address = s.recvfrom(2048)
+        pkt, addr = s.recvfrom(1472)
+        header = PacketHeader(pkt[:16])
 
-        # Extract header and payload
-        pkt_header = PacketHeader(pkt[:16])
-        msg = pkt[16 : 16 + pkt_header.length]
-
-        # Verity checksum
-        pkt_checksum = pkt_header.checksum
-        pkt_header.checksum = 0
-        computed_checksum = compute_checksum(pkt_header / msg)
-        if pkt_checksum != computed_checksum:
-            print("checksums not match")
-        print(msg)
-
+        if header.type == 0 and not connection_active:
+            # START received
+            connection_active = True
+            expected_seq = 1
+            # Reply with START ACK (seq 1)
+            ack = PacketHeader(type=3, seq_num=1, length=0)
+            ack.checksum = compute_checksum(ack / b'')
+            s.sendto(bytes(ack / b''), addr)
+            print("Sent START ACK", file=sys.stderr)
+            # After this commit, we can exit
+            break
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "receiver_ip", help="The IP address of the host that receiver is running on"
-    )
-    parser.add_argument(
-        "receiver_port", type=int, help="The port number on which receiver is listening"
-    )
-    parser.add_argument(
-        "window_size", type=int, help="Maximum number of outstanding packets"
-    )
+    parser.add_argument("receiver_ip")
+    parser.add_argument("receiver_port", type=int)
+    parser.add_argument("window_size", type=int)
     args = parser.parse_args()
-
     receiver(args.receiver_ip, args.receiver_port, args.window_size)
-
 
 if __name__ == "__main__":
     main()
--- RTP-base/sender.py
+++ RTP-base/sender.py
@@ -1,33 +1,42 @@
 import argparse
 import socket
-
+import sys
 from utils import PacketHeader, compute_checksum
 
-
 def sender(receiver_ip, receiver_port, window_size):
-    """TODO: Open socket and send message from sys.stdin."""
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    pkt_header = PacketHeader(type=2, seq_num=10, length=14)
-    pkt_header.checksum = compute_checksum(pkt_header / "Hello, world!\n")
-    pkt = pkt_header / "Hello, world!\n"
-    s.sendto(bytes(pkt), (receiver_ip, receiver_port))
+    print("Starting sender\n")
 
+    # Read all data from stdin (we won't actually send DATA yet)
+    data = sys.stdin.buffer.read()
+    print(f"Read {len(data)} bytes from stdin.")
+
+    # Send START packet
+    start_header = PacketHeader(type=0, seq_num=0, length=0)
+    start_header.checksum = compute_checksum(start_header / b'')
+    start_packet = bytes(start_header / b'')
+    s.sendto(start_packet, (receiver_ip, receiver_port))
+    print("Sent START")
+
+    # Wait for START ACK
+    s.settimeout(1)
+    try:
+        pkt, _ = s.recvfrom(1472)
+        ack_header = PacketHeader(pkt[:16])
+        if ack_header.type == 3 and ack_header.seq_num == 1:
+            print("Received START ACK")
+        else:
+            print("Did not receive proper START ACK")
+    except socket.timeout:
+        print("Timeout waiting for START ACK")
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "receiver_ip", help="The IP address of the host that receiver is running on"
-    )
-    parser.add_argument(
-        "receiver_port", type=int, help="The port number on which receiver is listening"
-    )
-    parser.add_argument(
-        "window_size", type=int, help="Maximum number of outstanding packets"
-    )
+    parser.add_argument("receiver_ip")
+    parser.add_argument("receiver_port", type=int)
+    parser.add_argument("window_size", type=int)
     args = parser.parse_args()
-
     sender(args.receiver_ip, args.receiver_port, args.window_size)
-
 
 if __name__ == "__main__":
     main()
